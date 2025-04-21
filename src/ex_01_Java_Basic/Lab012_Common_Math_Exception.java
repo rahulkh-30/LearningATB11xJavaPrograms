0fb18247d15947aa516aece2b6f99fc7aedc895c
Rahul <rahul30kh@gmail.com>
2025-04-21 19:40:23 +0530

    11 march

--- src/ex_01_Java_Basic/Lab012_Common_Math_Exception.java
+++ src/ex_01_Java_Basic/Lab012_Common_Math_Exception.java
@@ -5,6 +5,7 @@ public class Lab012_Common_Math_Exception {
     public static void main(String[] args) {
 
         System.out.println(10/0);
-        System.out.println(10/0);// "main" java.lang.ArithmeticException: / by zero
+        System.out.println(10/0);
+        // "main" java.lang.ArithmeticException: / by zero
     }
 }
