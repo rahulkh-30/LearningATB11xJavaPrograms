2e688ee5fd89a59c86833473f52dce6a9ab1c514
Rahul <rahul30kh@gmail.com>
2025-04-23 21:36:31 +0530

    14 march

---
+++ src/ex_07_Increment_Decrement_Op/Lab070_ID_Op.java
@@ -0,0 +1,37 @@
+package ex_07_Increment_Decrement_Op;
+
+public class Lab070_ID_Op {
+
+    public static void main(String[] args) {
+         // pre-increment--) ++opernd
+        //- value is incremented first and then stored in the result.
+        // value is increade by 1
+
+        int a = 10;
+        int b = ++a; // 10+1 -> b -> 11
+        System.out.println(b); // 11
+        System.out.println(a); // 11
+
+
+        //  Exp and Result Table
+        // Line No | a | Result b
+        // 10| 10 | NA
+        // 11| 11 | 11
+        // 12 | 11(NA) | 11
+        // 13 | 11 | 11(NA)
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
